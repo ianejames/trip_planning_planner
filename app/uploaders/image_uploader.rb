a3124eeedde9c1998fba38d141e15c8965478f75
Ian James <ianejames@yahoo.com>
2015-03-25 16:31:50 -0700

    Resize images to 360x360 on upload

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,7 +3,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+  include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
@@ -30,6 +30,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def scale(width, height)
   #   # do something
   # end
+
+  process :resize_to_fill => [360, 360]
 
   # Create different versions of your uploaded files:
   # version :thumb do
